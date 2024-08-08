bb39e43109e4465faa53cb0e52249e6b337889db
Gaurav Neupane <ngaurav456@gmail.com>
2024-08-08 15:24:47 -0500

    feat(overall): add prototype for vector db and embedding extrator

---
+++ src/feature_extraction/extractor.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+from keras.applications import VGG16
+from keras.models import Model
+
+
+
+def load_model():
+  '''load the vgg16 model from local directory or download it'''
+  base_model = VGG16(weights='imagenet', include_top=False)
+  model = Model(inputs=base_model.input, outputs = base_model.get_layer("block5_pool").output)
+  return model
+
+def embedding_extraction(image):
+  '''extract embedding of the image from vgg16'''
+  if image is None:
+    raise "image not loaded"
+  model = load_model()
+  image  = cv2.resize(image, (224,224))
+  image_expand = np.expand_dims(image, axis=0)
+  embedding = model.predict(image_expand)
+  return embedding
--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
 from utils import image
+from feature_extraction import extractor
+from retrieval import index_image, query_image
 
-image.check()
+image = image.load_image("/Users/gauravneupane/Documents/ml/projects/image_retrieval/src/sample/000001.jpg")
+embedding = extractor.embedding_extraction(image)
+
+# add image to the vector database
+index_image(embedding,"hello.txt")
+
+print(query_image(embedding))
